4d3c4430eb5c98038a99f720bd02e03c669920dc
Sudeep Saurabh <63743435+SuTiger6@users.noreply.github.com>
2023-10-25 22:16:32 -0400

    Add files via upload

--- project/Final/modules/globalvars/output.tf
+++ project/Final/modules/globalvars/output.tf
@@ -1,1 +1,12 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Sudeep"
+    "App"   = "Web"
+  }
+}
 
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment"
+}
